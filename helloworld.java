202b8f1f1584b13f984cc8b476bf8679fef63358
KalyanAzure <105334314+KalyanAzure@users.noreply.github.com>
2022-06-08 09:04:34 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World 8th june-update!");
+    System.out.println("Hello World 8th june-update-09:05AM!");
     }
 }
